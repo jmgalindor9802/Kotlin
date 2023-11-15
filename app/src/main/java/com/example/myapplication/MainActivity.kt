03048e3a770bef2b2f631a05850fc408b810e038
jmgalindor9802 <jmgalindor9802@gmail.com>
2023-11-15 11:34:31 -0500

    Clase 15Nov final

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.models.PelisClient
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +13,22 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val pelisAdapter = PelisAdapter(emptyList(),object :PelisClickedListener{
+            override fun onPelisClicked(peli: Result){
+                Toast.makeText(this@MainActivity, peli.original_title, Toast.LENGTH_LONG).show()
+            }
+        })
+
         binding.recycler.adapter
+
+        thread {
+            val api_key="";
+            val popPelis=PelisClient.service.listPopularPelis(api_key)
+            val body = popPelis.execute().body()
+            if (body!=null){
+                pelisAdapter.pelis=body.results
+                pelisAdapter.notifyDataSetChanged()
+            }
+        }
     }
 }
